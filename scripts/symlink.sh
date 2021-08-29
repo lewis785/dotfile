cbcfb8df864c84d14132a5e166980a3c8b3ddd4d
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2021-08-29 23:07:55 +0100

    Added symlink setup script

---
+++ scripts/symlink.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+dotfileDir=$(pwd | sed 's/\(dotfiles\).*/\1/g')
+
+function linkDotfile {
+  dest="${HOME}/${2}"
+  src="${dotfileDir}/${1}"
+  dateStr=$(date +%Y-%m-%d-%H%M)
+
+  if [ -h ~/${2} ]; then
+    # Existing symlink
+    echo "Removing existing symlink: ${dest}"
+    rm ${dest}
+  elif [ -f "${dest}" ]; then
+    # Existing file
+    echo "Backing up existing file: ${dest}"
+    mv ${dest}{,.${dateStr}}
+
+  elif [ -d "${dest}" ]; then
+    # Existing dir
+    echo "Backing up existing dir: ${dest}"
+    mv ${dest}{,.${dateStr}}
+  fi
+
+  echo "Creating new symlink: ${dest}"
+  ln -s ${src} ${dest}
+}
+
+linkDotfile git/.gitconfig .gitconfig
+linkDotfile zsh/.zshrc .zshrc
+linkDotfile zsh/.p10k.zsh .p10k.zsh
