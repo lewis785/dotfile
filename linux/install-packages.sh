f23aa2b74863b2d40f23c7a44c771306c14b6b3c
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2023-04-26 16:29:45 +0100

    REFACTOR move linux package install to separate script

---
+++ linux/install-packages.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+packages=(
+	awscli
+	bat
+	curl
+	exa
+	fzf
+	git
+	pandoc
+	vim
+	zsh
+)
+
+function install_package() {
+	if dpkg --list | grep -w $1 -q; then
+		echo "Already installed: $1"
+	else
+		echo ">>> Installing $1"
+		sudo apt install $1 -y
+		echo ""
+	fi
+}
+
+for i in "${packages[@]}"; do
+	install_package "$i"
+done
--- linux/linux-setup.sh
+++ linux/linux-setup.sh
@@ -3,18 +3,8 @@
 echo -e "\n===Setting up symlinks===\n"
 ../common/symlink.sh
 
-echo -e "\n===Installing programs===\n"
-
-sudo apt install \
-	awscli \
-	bat\
-	curl\
-	exa\
-	fzf \
-	git \
-	pandoc\
-	vim \
-	-y
+echo -e "\n===Installing packages===\n"
+./install-packages.sh
 
 echo -e "\n===Installing applications===\n"
 
@@ -38,10 +28,9 @@ gsettings set org.gnome.shell.extensions.dash-to-dock multi-monitor true
 gsettings set org.gnome.desktop.session idle-delay 1800
 
 echo -e "\n===Setting up zsh===\n"
-if command -v zsh &> /dev/null; then
-	echo -e "ZSH already installed"
+if [ -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
+	echo "Oh My Zsh already installed"
 else
-	sudo apt install zsh
 	sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
 	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 	chsh -s $(which zsh)
